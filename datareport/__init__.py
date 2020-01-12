b9477bee775635ba0a8dd0a9f268a2bcbbba22de
Gordon Chen <cgd.muye@gmail.com>
2020-01-12 12:37:52 -0600

    change print_report to a static function

--- datareport/__init__.py
+++ datareport/__init__.py
@@ -1,6 +1,6 @@
 """The :mod: `dsdk.data_report` module prints a report including statistics and plots for a given dataset."""
 
-from .descriptive_report import DataReport
+from .descriptive_report import DataReport, print_report
 from .viz import plot_time_series
 from .collect_stats import get_data_type, get_variable_stats
 
--- datareport/descriptive_report.py
+++ datareport/descriptive_report.py
@@ -7,6 +7,44 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from .collect_stats import get_variable_stats, get_table_stats, get_a_sample
 
 
+def print_report(df: pd.DataFrame,
+                 prt_table_stats: bool = True,
+                 prt_var_stats: bool = True,
+                 sample_size: int = 15000,
+                 var_per_row: int = 6,
+                 random_state: int = 0) -> None:
+    """
+    Print out descriptive analysis report for a given dataset.
+
+    :param df: the target dataset
+    :param prt_table_stats: if print the table statistics
+    :param prt_var_stats: if print the variables statistics
+    :param prt_corr_stats: if print the correlation table
+    :param prt_plots: if print plots
+    :param sample_size: Number of rows to sample from the target dataframe
+    :param var_per_row: number of columns of stats to print per row
+    :param random_state: Random seed for the row sampler
+    :return: None
+    """
+    sample_df = get_a_sample(df, sample_size, random_state)
+
+    var_stats = get_variable_stats(sample_df)
+
+    if prt_table_stats:
+        table_stats = get_table_stats(sample_df, var_stats)
+        print('\n=============Table Statistics ============== \n')
+        print(tabulate(pd.DataFrame(pd.Series(table_stats), columns=['count']), headers='keys', tablefmt='psql'))
+
+    if prt_var_stats:
+        print('\n===============Variables Statistics ============ ')
+        for key, item in var_stats.items():
+            print(f'\nSummary of {key} variables: \n')
+            for i in range(len(var_stats[key]) // var_per_row + 1):
+                print(tabulate(
+                    pd.DataFrame(item).drop('type', axis=1).T.iloc[:, (i) * var_per_row:(i + 1) * var_per_row], \
+                    headers='keys', tablefmt='psql'))
+
+
 class DataReport(BaseEstimator, TransformerMixin):
     """
     This class is designed to print reports of statistics and plots.
@@ -16,44 +54,6 @@ class DataReport(BaseEstimator, TransformerMixin):
 
     def __init__(self) -> None:
         """Initialization."""
-
-    def print_report(self,
-                     df: pd.DataFrame,
-                     prt_table_stats: bool = True,
-                     prt_var_stats: bool = True,
-                     sample_size: int = 15000,
-                     var_per_row: int = 6,
-                     random_state: int = 0) -> None:
-        """
-        Print out descriptive analysis report for a given dataset.
-
-        :param df: the target dataset
-        :param prt_table_stats: if print the table statistics
-        :param prt_var_stats: if print the variables statistics
-        :param prt_corr_stats: if print the correlation table
-        :param prt_plots: if print plots
-        :param sample_size: Number of rows to sample from the target dataframe
-        :param var_per_row: number of columns of stats to print per row
-        :param random_state: Random seed for the row sampler
-        :return: None
-        """
-        sample_df = get_a_sample(df, sample_size, random_state)
-
-        var_stats = get_variable_stats(sample_df)
-
-        if prt_table_stats:
-            table_stats = get_table_stats(sample_df, var_stats)
-            print('\n=============Table Statistics ============== \n')
-            print(tabulate(pd.DataFrame(pd.Series(table_stats), columns=['count']), headers='keys', tablefmt='psql'))
-
-        if prt_var_stats:
-            print('\n===============Variables Statistics ============ ')
-            for key, item in var_stats.items():
-                print(f'\nSummary of {key} variables: \n')
-                for i in range(len(var_stats[key]) // var_per_row + 1):
-                    print(tabulate(
-                        pd.DataFrame(item).drop('type', axis=1).T.iloc[:, (i) * var_per_row:(i + 1) * var_per_row], \
-                        headers='keys', tablefmt='psql'))
 
     def fit(self, X: pd.DataFrame, y: pd.Series = None, **kwargs) -> pd.DataFrame:
         """
@@ -73,7 +73,7 @@ class DataReport(BaseEstimator, TransformerMixin):
         :param y: y
         :return: the original dataset
         """
-        self.print_report(X, **kwargs)
+        print_report(X, **kwargs)
         return X
 
     def fit_transform(self, X: pd.DataFrame, y: pd.Series = None, **kwargs) -> pd.DataFrame:
