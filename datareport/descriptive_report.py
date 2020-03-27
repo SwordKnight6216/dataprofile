6896fe2d2d52d22f6e0210a846b099e183d3bc95
Gordon Chen <cgd.muye@gmail.com>
2020-03-27 16:13:48 -0500

    add new feature to save report to txt or html format

--- datareport/descriptive_report.py
+++ datareport/descriptive_report.py
@@ -9,8 +9,8 @@ import click
 import pandas as pd
 from tabulate import tabulate
 
-from datareport.config import DEFAULT_SAMPLE_SIZE, AUTHOR, RANDOM_STATE
 from datareport.collect_stats import get_variable_stats, get_table_stats, get_a_sample
+from datareport.config import DEFAULT_SAMPLE_SIZE, AUTHOR, RANDOM_STATE
 
 
 def print_report(df: pd.DataFrame,
@@ -36,25 +36,30 @@ def print_report(df: pd.DataFrame,
     :param report_file: store the report to a file
     :return: None
     """
-
-    sample_df = get_a_sample(df, sample_size, random_state)
-
-    var_stats = get_variable_stats(sample_df)
+    table_fmt = 'psql'
+    line_ending = '\n'
 
     if report_file:
+        if report_file.split('.')[-1] == 'html':
+            table_fmt = 'html'
+            line_ending = '<br>'
         report_file = _make_report_file(report_file)
         file = open(report_file, 'w', encoding="UTF-8")
     else:
         file = None
 
+    sample_df = get_a_sample(df, sample_size, random_state)
+
+    var_stats = get_variable_stats(sample_df)
+
     try:
-        print('\n============= Beginning of report ============ ', file=file)
-        print(f"\nThe following report is created by {AUTHOR} on {date.today()}", file=file)
+        print('\n============= Beginning of report ============ ', file=file, end=line_ending)
+        print(f"\nThe following report is created by {AUTHOR} on {date.today()}", file=file, end=line_ending)
         if prt_table_stats:
             table_stats = get_table_stats(sample_df, var_stats)
-            print('\n============= Table Statistics ============== \n', file=file)
-            print(tabulate(pd.DataFrame(pd.Series(table_stats), columns=['count']), headers='keys', tablefmt='psql'),
-                  file=file)
+            print('\n============= Table Statistics ============== \n', file=file, end=line_ending)
+            print(tabulate(pd.DataFrame(pd.Series(table_stats), columns=['count']), headers='keys', tablefmt=table_fmt),
+                  file=file, end=line_ending)
 
         if prt_var_summary:
             type_stats = ['type', 'count', 'distinct_count', 'p_unique', 'n_missing', 'p_missing']
@@ -63,20 +68,20 @@ def print_report(df: pd.DataFrame,
                 tmp_df_stats.append(pd.DataFrame(item)[type_stats])
             var_summary = pd.concat(tmp_df_stats)
             var_summary.index.name = 'name'
-            print('\n============= Variable Summary ============== \n', file=file)
-            print(tabulate(var_summary, headers='keys', tablefmt='psql'), file=file)
+            print('\n============= Variable Summary ============== \n', file=file, end=line_ending)
+            print(tabulate(var_summary, headers='keys', tablefmt=table_fmt), file=file, end=line_ending)
 
         if prt_var_stats:
-            print('\n============= Variable Statistics =========== ', file=file)
+            print('\n============= Variable Statistics =========== ', file=file, end=line_ending)
             for key, item in var_stats.items():
-                print(f'\n{key} variables: \n', file=file)
+                print(f'\n{key} variables: \n', file=file, end=line_ending)
                 for i in range(len(var_stats[key]) // (var_per_row + 1) + 1):
                     print(tabulate(
                         pd.DataFrame(item).drop('type', axis=1).T.iloc[:, i * var_per_row:(i + 1) * var_per_row],
-                        headers='keys', tablefmt='psql'), file=file)
+                        headers='keys', tablefmt=table_fmt), file=file, end=line_ending)
 
         print(f"report saved to {report_file}") if file else None
-        print('\n=============== End of report ============ ', file=file)
+        print('\n=============== End of report ============ ', file=file, end=line_ending)
 
     except:
         print('Report not finished successfully!')
@@ -119,9 +124,10 @@ def _find_csv_file() -> Optional[Path]:
 @click.option('--var_per_row', prompt='How many variables to show per row?', required=False, default=6,
               show_default=True,
               help='number of variables to show per row')
-@click.option('--save_report_to_file', prompt='file to store the report', required=False, default='',
+@click.option('--save_report_to_file', prompt='file type to store the report, skip if not needed or choose one from',
+              required=False, default='', type=click.Choice(['', 'html', 'txt']),
               show_default=True,
-              help='file to store the report')
+              help='file type (html or txt) to store the report, skip if not needed')
 def main(path: str, prt_table_stats: bool = True,
          prt_var_stats: bool = True,
          sample_size: int = DEFAULT_SAMPLE_SIZE,
@@ -141,8 +147,10 @@ def main(path: str, prt_table_stats: bool = True,
     except FileNotFoundError:
         print("Target file doesn't exist or not CSV format! \nReporting stopped!")
         sys.exit(1)
+    report_file_name = str(path).split('/')[-1].split('.')[
+                           0] + '_report.' + save_report_to_file if save_report_to_file else None
     print_report(df, prt_table_stats=prt_table_stats, prt_var_stats=prt_var_stats, sample_size=sample_size,
-                 var_per_row=var_per_row, report_file=save_report_to_file)
+                 var_per_row=var_per_row, report_file=report_file_name)
 
 
 if __name__ == "__main__":
