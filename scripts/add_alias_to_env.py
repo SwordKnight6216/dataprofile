3bfa3307f3a6565dd3e5efa68cf8adb5cfeae626
Gordon Chen <cgd.muye@gmail.com>
2020-04-02 18:12:01 -0500

    add a script to append the datareport alias to dotfile

---
+++ scripts/add_alias_to_env.py
@@ -0,0 +1,18 @@
+import sys
+from pathlib import Path
+
+py_path = sys.executable
+current_path = Path('.').absolute()
+cmd = f"alias datareport=\"{py_path} {Path(current_path, 'cli_report.py')}\""
+
+dotfiles = ['.zshrc', '.bashrc', '.bash_profile']
+
+for file in dotfiles:
+    dotfile = Path(Path().home(), file)
+    try:
+        with open(dotfile, "a") as f:
+            f.write(cmd)
+            print(f"successfully create the cmd alias datareport in {dotfile}")
+            break
+    except:
+        pass
