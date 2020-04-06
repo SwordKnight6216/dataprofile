49e6d5baa10b730aea2b3f5da0f7c771846f91bc
Gordon Chen <cgd.muye@gmail.com>
2020-04-06 12:23:48 -0500

    bash code improvement

--- docs/docker/build_docker.sh
+++ docs/docker/build_docker.sh
@@ -1,1 +1,8 @@
-docker build ../ -t dataprofile_web
+#!/bin/bash
+
+html=../build/html
+if [ -d "$html" ]; then
+    docker build ../ -t dataprofile_web
+else
+    echo "please make html files before build docker"
+fi
