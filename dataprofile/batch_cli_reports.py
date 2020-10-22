43654769877c27d1868fde1976f7734628907f79
Gordon Chen <cgd.muye@gmail.com>
2020-10-22 17:41:22 -0500

    initial render_reports_for_all

--- dataprofile/batch_cli_reports.py
+++ dataprofile/batch_cli_reports.py
@@ -4,9 +4,11 @@ import os
 import sys
 from typing import List, Tuple
 
+import pandas as pd
 from loguru import logger
 
 from ._config import LOG_FILE
+from .reporting import render_report
 
 logger.remove()
 logger.add(sys.stdout, format="{time:YYYY-MM-DD at HH:mm:ss}|{level}|{message}", level="INFO")
@@ -39,10 +41,31 @@ def find_files(target_dir: str = os.getcwd(), file_suffix: str = ".csv") -> List
     output = []
     for subdir, dirs, files in os.walk(target_dir):
         for filename in files:
-            filepath = os.path.join(subdir, filename)
-            filepath = "." + filepath[len(target_dir):]
+            filepath_full = os.path.join(subdir, filename)
+            filepath = "." + filepath_full[len(target_dir):]
             if filepath.endswith(file_suffix):
-                f_size_original = os.path.getsize(filepath)
+                f_size_original = os.path.getsize(filepath_full)
                 f_size = _human_readable_size(f_size_original)
-                output.append((f_size, f_size_original, filepath))
+                output.append((f_size, f_size_original, filepath_full))
     return output
+
+
+def render_reports_for_all(target_dir: str = os.getcwd(), file_suffix: str = ".csv", report_type: str = ".txt"):
+    files = find_files(target_dir, file_suffix)
+    for f in files:
+        print(f)
+    text = input(f"Continue to generate reports for the {len(files)} files? Y/N") or "N"
+    if text == "Y":
+        cnt = 0
+        for _, _, f in files:
+            try:
+                df = pd.read_csv(f)
+                report_file = f[:-len(file_suffix)] + report_type
+                logger.info(f"\nRender Report for {f}...")
+                render_report(df, report_file=report_file)
+                cnt += 1
+            except UnicodeDecodeError:
+                logger.warning(f"{f} skipped due to UnicodeDecodeError!")
+        logger.info(f"Summary: {cnt} reports successfully rendered, {len(files) - cnt} failed.")
+    else:
+        print("Aborted!")
